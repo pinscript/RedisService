52c0a2f59a934f29d8128bfa12c3fef97b5de072
Alexander Nyquist <nyquist.alexander@gmail.com>
2013-06-23 15:22:12 +0200

    Initial commit

---
+++ RedisService/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using Topshelf;
+
+namespace RedisService
+{
+    public class Program
+    {
+        public static void Main(string[] argv)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<RedisServerWrapper>(s =>
+                {
+                    var pathToRedisExecutable = ConfigurationManager.AppSettings["PathToRedisExecutable"];
+
+                    s.ConstructUsing(_ => new RedisServerWrapper(pathToRedisExecutable));
+                    s.WhenStarted(rsw => rsw.Start());
+                    s.WhenStopped(rsw => rsw.Stop());
+                    s.WhenShutdown(rsw => rsw.Stop());
+                   
+                });
+
+                x.RunAsLocalSystem();
+                x.StartAutomatically();
+                x.SetDescription("Wraps redis-server.exe to avoid having a console application running for a specific user.");
+                x.SetDisplayName("RedisService");
+                x.SetServiceName("RedisService");
+            });
+        }        
+    }
+}
---
+++ RedisService/RedisServerWrapper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace RedisService
+{
+    public class RedisServerWrapper
+    {
+        private readonly string _pathToRedisExecutable;
+        private readonly string _pathToRedisDirectory;
+        private Process _process;
+
+        public RedisServerWrapper(string pathToRedisExecutable)
+        {
+            _pathToRedisExecutable = pathToRedisExecutable;
+            _pathToRedisDirectory = new FileInfo(pathToRedisExecutable).DirectoryName;         
+        }
+
+        /// <summary>
+        /// Called when service is started. Invokes redis-server.exe.
+        /// </summary>
+        public void Start()
+        {
+            var processStartInfo = new ProcessStartInfo(_pathToRedisExecutable);
+            processStartInfo.WorkingDirectory = _pathToRedisDirectory;
+
+            _process = Process.Start(processStartInfo);
+        }
+
+        /// <summary>
+        /// Called when service is stopped. Kills all redis-serve.exe processes.
+        /// </summary>
+        public void Stop()
+        {
+            KillProcess(_process);
+
+            // Try to check for redis-server.exe just in case
+            var processes = Process.GetProcessesByName("redis-server.exe");
+            if (processes.Length > 0)
+            {
+                foreach (var process in processes)
+                {
+                    KillProcess(process);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Kills a specific process.
+        /// </summary>
+        /// <param name="process">Process to kill</param>
+        private void KillProcess(Process process)
+        {
+            if (process != null && !process.HasExited)
+            {
+                try
+                {
+                    process.CloseMainWindow();
+                }
+                catch (Exception)
+                {
+                    process.Kill();
+                }
+            }
+        }
+    }
+}
