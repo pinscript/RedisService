ba8be8711e922953f2be0f098568c01520e2ca9f
Alexander Nyquist <nyquist.alexander@gmail.com>
2013-08-27 08:49:55 +0200

    Added AssemblyInfo.cs

---
+++ RedisService/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RedisService")]
+[assembly: AssemblyDescription("Wraps redis-server.exe to avoid having a console application running for a specific user.")]
+[assembly: AssemblyProduct("RedisService")]
+[assembly: AssemblyCopyright("Copyright © Alexander Nyquist  2013")]
+[assembly: ComVisible(false)]
+[assembly: Guid("23378238-6566-4106-aaa2-a3f0e00f7c6f")]
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyFileVersion("0.1")]
